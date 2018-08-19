f94c62999ea44f8f6f8c4263a2d4486324f0268d
noribang <hoonk9387@gmail.com>
2018-08-19 01:16:55 -0500

    Started new bmi.js file.

---
+++ 02_bmi.js
@@ -0,0 +1,8 @@
+////////////////////////////////////////////////////////
+// Body Mass index
+////////////////////////////////////////////////////////
+let weight = parseInt(prompt("Enter weight in kilograms: "));
+let height = parseInt(prompt("Enter height in metersssss: "));
+
+console.log(weight + " is type: " + typeof(weight));
+console.log(height + " is type: " + typeof(height));
